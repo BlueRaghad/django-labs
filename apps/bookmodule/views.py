ec37823e93d66883187a7f84efc192441527f695
blueRaghad <rtrag1231234@gmail.com>
2026-03-31 19:54:36 +0300

    task4 done with loading statics

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,32 +1,22 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-# Create your views here.
+def index(request):
+    return render(request, "bookmodule/index.html")
 
-#def index(request):
-#    name = request.GET.get("name") or "World!"
-    #return HttpResponse("hello, " + name)
-   # return render(request, "bookmodule/index.html", {"name": name})
+def index2(request, val=0):
+    return render(request, "bookmodule/index2.html", {"val": val})
 
-def index2(request, val = 0):
-    return render(request, "bookmodule/index2.html",{"val": val})
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
 
-def viewbook(request, bookID):
+def viewbook(request, bookId):        # ← pick bookId and stick with it
     book1 = {'id': 123, 'title': 'onePiece vol1', 'author': 'oda'}
     book2 = {'id': 124, 'title': 'onePiece vol2', 'author': 'oda'}
-
     targetBook = None
-    if book1['id'] == bookID: targetBook = book1
-    if book2['id'] == bookID: targetBook = book2
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
     context = {'book': targetBook}
     return render(request, 'bookmodule/show.html', context)
 
-
-def index(request):
-    return render(request, "bookmodule/index.html")
-def list_books(request):
-    return render(request, 'bookmodule/list_books.html')
-def viewbook(request, bookId):
-    return render(request, 'bookmodule/one_book.html')
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
